c5143b47f920604cc99fea7249445842ab38bc51
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-03-17 20:25:57 -0400

    Updates

---
+++ precache-manifest.3b48814979ffa956c01f21d0edc78efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4eecb6791a67d8b4c3771a0bd79f67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8ba785034387af32e77",
+    "url": "/static/css/main.97f1e7cb.chunk.css"
+  },
+  {
+    "revision": "7fbdf5e93f315c01d371",
+    "url": "/static/js/2.361b9358.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.361b9358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ba785034387af32e77",
+    "url": "/static/js/main.4c1258e5.chunk.js"
+  },
+  {
+    "revision": "9738d6aaee37b7b93854",
+    "url": "/static/js/runtime-main.707ea816.js"
+  }
+]);
